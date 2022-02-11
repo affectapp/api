edde2783c43c824899e5576a595a57a3c2e1f732
Keenan Thompson <keenan@keenant.com>
2022-02-10 18:23:24 -0800

    Add initial protos and rust project.

---
+++ build.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_files = ["proto/affect/metadata.proto", "proto/affect/user.proto"];
+
+    // Rebuild protos if any of the files change.
+    for proto_file in proto_files {
+        println!("cargo:rerun-if-changed={0}", proto_file);
+    }
+
+    // Build protos.
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
+    let file_descriptor = out_dir.join("proto.bin");
+    tonic_build::configure()
+        .format(false)
+        .file_descriptor_set_path(file_descriptor)
+        .compile(&proto_files, &["proto"])?;
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("proto");
+
+pub mod affect {
+    tonic::include_proto!("affect");
+}
