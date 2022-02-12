b573b4730b8e3465bfc29ac6f1d5e8a2909609b2
Keenan Thompson <keenan@keenant.com>
2022-02-12 12:34:33 -0800

    Move generated to dist.

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@ PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 PROTOC_PATH="./node_modules/.bin/protoc"
 
 # Directory to write generated code to (.js and .d.ts files)
-OUT_DIR="./generated"
+OUT_DIR="./dist/generated"
 
+rm -r $OUT_DIR
 mkdir -p ${OUT_DIR}
 ${PROTOC_PATH} \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
