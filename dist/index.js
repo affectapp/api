c1b07896810b3a7ef302a26d55a52311b0f111f8
Keenan Thompson <keenan@keenant.com>
2022-02-18 00:21:02 -0800

    Fix ts exporting protos.

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,8 @@ exports.affect = void 0;
 const metadata_pb = require("../dist/generated/affect/metadata_pb");
 const user_pb = require("../dist/generated/affect/user_pb");
 const user_pb_service = require("../dist/generated/affect/user_pb_service");
-var affect;
-(function (affect) {
-    metadata_pb;
-    user_pb;
-    user_pb_service;
-})(affect = exports.affect || (exports.affect = {}));
+exports.affect = {
+    metadata_pb: metadata_pb,
+    user_pb: user_pb,
+    user_pb_service: user_pb_service,
+};
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as user_pb from "../dist/generated/affect/user_pb";
 import * as user_pb_service from "../dist/generated/affect/user_pb_service";
 
 export const affect = {
-  metadata_pb,
-  user_pb,
-  user_pb_service,
+  metadata_pb: metadata_pb,
+  user_pb: user_pb,
+  user_pb_service: user_pb_service,
 };
