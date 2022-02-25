f37034f3e96563416e688b67b624272830d24c90
Keenan Thompson <keenan@keenant.com>
2022-02-25 15:57:16 -0800

    Fix rust proto building.

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,10 @@
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_files = ["proto/affect/metadata.proto", "proto/affect/user.proto"];
+    let proto_files = [
+        "../proto/affect/metadata.proto",
+        "../proto/affect/user.proto",
+    ];
 
     // Rebuild protos if any of the files change.
     for proto_file in proto_files {
@@ -14,6 +17,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .format(false)
         .file_descriptor_set_path(file_descriptor)
-        .compile(&proto_files, &["proto"])?;
+        .compile(&proto_files, &["../proto"])?;
     Ok(())
 }
