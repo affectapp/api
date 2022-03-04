493ca2285df00908cdc36491b162a460ea1b7c68
Keenan Thompson <keenan@keenant.com>
2022-03-03 22:54:27 -0800

    Add item client to ts.

--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,13 @@
 import { GrpcWebClientBaseOptions } from "grpc-web";
+import { ItemServiceClient } from "./generated/affect/ItemServiceClientPb";
 import { UserServiceClient } from "./generated/affect/UserServiceClientPb";
 
 export default class AffectApi {
   user: UserServiceClient;
+  item: ItemServiceClient;
 
   constructor(serviceHost: string, options?: GrpcWebClientBaseOptions) {
     this.user = new UserServiceClient(serviceHost, null, options);
+    this.item = new ItemServiceClient(serviceHost, null, options);
   }
 }
-export * as user_pb from './generated/affect/user_pb';
-export * as metadata_pb from './generated/affect/metadata_pb';
